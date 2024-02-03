40abeeb3d472b308f82489a8b46e9a0ab5980cd7
John Salt <js730674@sju.edu>
2024-02-03 14:25:42 -0500

    added profile image

--- lib/profile.dart
+++ lib/profile.dart
@@ -4,7 +4,40 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("PROFILE PAGE"),
+      body: Container(
+        //Adds padding around the edges
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: AlignmentDirectional.topStart,
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(60.0),
+                    child: const Image(
+                      image: AssetImage("lib/assets/empty_user.png"),
+                      height: 100.0,
+                      width: 100.0,
+                    ),
+                  ),
+                  const Align(
+                    alignment: AlignmentDirectional.topStart,
+                    child: Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("John Salt"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

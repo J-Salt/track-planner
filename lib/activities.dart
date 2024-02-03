d855da82b2a33ca6bdf3cdd54c411be19ec6402b
John Salt <js730674@sju.edu>
2024-02-02 22:20:03 -0500

    added basic pages

---
+++ lib/activities.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Activities extends StatelessWidget {
+  const Activities({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'ACTIVITES PAGE',
+          ),
+        ],
+      ),
+    );
+  }
+}

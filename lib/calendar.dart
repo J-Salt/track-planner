a2d14ecb202570976322ed10ec9762d4bf438be0
John Salt <js730674@sju.edu>
2024-02-13 13:50:45 -0500

    basic calendar functionality

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+final selectedDayProvider = StateProvider<DateTime>((ref) => DateTime.now());
+
+class Calendar extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    DateTime now = DateTime.now();
+
+    final selectedDay = ref.watch(selectedDayProvider);
     return Scaffold(
-      body: Text("CALENDAR PAGE"),
+      body: TableCalendar(
+        firstDay: DateTime(
+          now.year,
+          now.month - 1,
+        ),
+        lastDay: DateTime(now.year, now.month + 2, 0),
+        focusedDay: now,
+        selectedDayPredicate: (day) {
+          final selectedDay = ref.read(selectedDayProvider.notifier).state;
+          return isSameDay(day, selectedDay);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          ref.read(selectedDayProvider.notifier).state = selectedDay;
+        },
+        headerStyle: const HeaderStyle(
+          formatButtonVisible: false,
+          titleCentered: true,
+        ),
+        calendarBuilders: CalendarBuilders(
+          todayBuilder: (context, date, _) {
+            final isSelected = isSameDay(date, selectedDay);
+            return Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isSelected ? Colors.blue : null,
+              ),
+              child: Text(
+                date.day.toString(),
+                style: TextStyle(
+                  color: isSelected ? Colors.white : null,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }

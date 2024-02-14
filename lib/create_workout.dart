a05a67b76ce0db3860f89f75c0c7bd564a9d6a5e
John Salt <js730674@sju.edu>
2024-02-13 21:16:37 -0500

    small additions

--- lib/create_workout.dart
+++ lib/create_workout.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:track_planner/calendar.dart';
+import 'package:track_planner/utils/reusable_appbar.dart';
 
 class CreateWorkout extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Create"),
+      appBar: ReusableAppBar(
+        context: context,
+        pageTitle: "Create Workout",
+        trailingActions: [
+          IconButton(
+            onPressed: () => {},
+            icon: const Icon(Icons.check),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(children: []),
+        child: Column(children: [
+          Text(ref.read(selectedDayProvider.notifier).state.toString())
+        ]),
       ),
     );
   }

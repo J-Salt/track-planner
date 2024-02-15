69001768cebec7d75842324eaa162862bcb0d92c
John Salt <js730674@sju.edu>
2024-02-15 18:58:29 -0500

    added db functionality

--- lib/create_workout.dart
+++ lib/create_workout.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:track_planner/calendar.dart';
+import 'package:track_planner/service.dart';
 import 'package:track_planner/utils/reusable_appbar.dart';
 
 class CreateWorkout extends ConsumerWidget {
+  CreateWorkout({super.key});
+
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descController = TextEditingController();
+  final DateFormat titleFormat = DateFormat("EEE, MMM d");
+  List<Object> sets = [
+    {
+      "test": "test",
+    },
+    {
+      "test": "test",
+    }
+  ];
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: ReusableAppBar(
         context: context,
-        pageTitle: "Create Workout",
+        pageTitle:
+            titleFormat.format(ref.read(selectedDayProvider.notifier).state),
         trailingActions: [
           IconButton(
-            onPressed: () => {},
+            onPressed: () => {
+              Service().createActivity(
+                  titleController.text, descController.text, sets)
+            },
             icon: const Icon(Icons.check),
           ),
         ],
@@ -19,7 +38,15 @@ class CreateWorkout extends ConsumerWidget {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(children: [
-          Text(ref.read(selectedDayProvider.notifier).state.toString())
+          const Text("Title:"),
+          TextField(
+            controller: titleController,
+          ),
+          const Text("Description:"),
+          TextField(
+            controller: descController,
+          ),
+          ElevatedButton(onPressed: () => {}, child: const Text("Add Set"))
         ]),
       ),
     );
--- lib/service.dart
+++ lib/service.dart
@@ -1,18 +1,11 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:uuid/uuid.dart';
 
 /// Service class with functions for editing firebase entities
 class Service {
   late DatabaseReference db;
 
-  /// Creates user with [id] and [user].
-  void createUser(String id, user) async {
-    db = FirebaseDatabase.instance.ref("users/$id");
-    await db
-        .set(user)
-        .then((_) => print("success!"))
-        .catchError((err) => {print(err)});
-  }
-
+  /// Creates firebase entry for user with [uid] from the users authentication.
   void registerUser(
       String uid, String name, String group, int gradYear, bool isCoach) async {
     db = FirebaseDatabase.instance.ref("users/$uid");
@@ -26,5 +19,22 @@ class Service {
         })
         .then((_) => print("success!"))
         .catchError((err) => {print(err)});
+  }
+
+  // TODO persist sets and add the id's to the parent activity
+  void createActivity(String title, String desc, List<Object> sets) async {
+    var uuid = const Uuid();
+    List<String> setIds = [];
+
+    for (Object set in sets) {
+      String id = uuid.v4();
+      db = FirebaseDatabase.instance.ref("sets/$id");
+      await db.set(set);
+      setIds.add(id);
+    }
+
+    db = FirebaseDatabase.instance.ref("activities/${uuid.v4()}");
+
+    await db.set({"title": title, "description": desc, "sets": setIds});
   }
 }

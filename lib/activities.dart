b7dd93d41b8aa5b98c86931568e59c9283a97a9c
John Salt <js730674@sju.edu>
2024-04-17 11:05:41 -0400

    fixed issue with not emptying the list on rebuild

--- lib/activities.dart
+++ lib/activities.dart
@@ -1,53 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:track_planner/auth.dart';
 import 'package:track_planner/preview_workout.dart';
 import 'package:track_planner/service.dart';
 import 'package:track_planner/utils/reusable_appbar.dart';
 import 'package:track_planner/utils/workout.dart';
 
-class Activities extends StatelessWidget {
-  static List<DisplayWorkout> friends_activites = [];
+class Activities extends StatefulWidget {
+  const Activities({super.key});
+
+  @override
+  _ActivitiesState createState() => _ActivitiesState();
+}
+
+class _ActivitiesState extends State<Activities> {
+  late Future<List<DisplayWorkout>> _friends_activites;
   static List<PreviewWorkout> workouts = [];
-  Activities({super.key});
+
+  final User user = Auth().currentUser!;
+  void getWorkouts() async {
+    _friends_activites = Service().getFriendsWorkouts(user.uid);
+  }
+
+  @override
+  void initState() {
+    getWorkouts();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    Future<List<DisplayWorkout>> _friends_activites = Service().getFriendsWorkouts("OzsRkaJIeddj2CX2SSx0jeamGmC3");
     return Scaffold(
-      appBar: ReusableAppBar(
-        pageTitle: "Activities",
-        context: context,
-        trailingActions: [
-          IconButton(
-              onPressed: () async {
-                _friends_activites = Service().getFriendsWorkouts("OzsRkaJIeddj2CX2SSx0jeamGmC3");
-              },
-              icon: Icon(Icons.person))
-        ],
-      ),
-      body: Expanded(
-        child: FutureBuilder(
-          future: _friends_activites,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return const Center(
-                child: Text("Error loading data"),
-              );
-            } else {
-              friends_activites = snapshot.data!;
-              for (DisplayWorkout activity in friends_activites) {
-                workouts.add(PreviewWorkout(workout: activity));
-              }
-              return Column(
-                  children: workouts
-              );
-            };
-          }
+        appBar: ReusableAppBar(
+          pageTitle: "Activities",
+          context: context,
         ),
-      )
-    );
+        body: Expanded(
+          child: FutureBuilder(
+              future: _friends_activites,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.hasError) {
+                  return const Center(
+                    child: Text("Error loading data"),
+                  );
+                } else {
+                  List<DisplayWorkout> friends_activites =
+                      snapshot.data as List<DisplayWorkout>;
+                  workouts = [];
+                  for (DisplayWorkout activity in friends_activites) {
+                    workouts.add(PreviewWorkout(workout: activity));
+                  }
+                  return ListView(
+                    shrinkWrap: true,
+                    children: workouts,
+                  );
+                }
+                ;
+              }),
+        ));
   }
 }
